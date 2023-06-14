db6d248fb7dbb06f21239dcb8195afd2775fbe72
Yer Yang <yyhmg_86@yahoo.com>
2023-06-14 14:05:10 -0700

    Select Search Term and Save to Variable and add event listener

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+console.log('main.js connected')
 const handleFormInputFocus = (event) => {
     console.log("focus occured")
 }
+const searchTermsInput = document.body.querySelector("#search-terms")
+searchTermsInput.addEventListener("focus", handleFormInputFocus)
