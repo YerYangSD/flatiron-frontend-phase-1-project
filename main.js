00162ec8ce53c9a30c39468c769d1cdb18244966
Yer Yang <yyhmg_86@yahoo.com>
2023-06-15 00:10:49 -0700

    reorganize dom form input select & handleFormFocus

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 console.log('main.js connected')
-const handleFormInputFocus = (event) => {
+const handleFormInputFocus = async (event) => {
     console.log("focus occured")
+    await getMealCategories()
 }
 const searchTermsInput = document.body.querySelector("#search-terms")
 searchTermsInput.addEventListener("focus", handleFormInputFocus)
