35c34d883edac1188c848e431b13e1e129133ba2
Yer Yang <yyhmg_86@yahoo.com>
2023-06-15 00:13:57 -0700

    fetch mealCategories data

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 console.log('main.js connected')
+const getMealCategories = async () => {
+    const mealCategoriesApiURL = "https://www.themealdb.com/api/json/v1/1/categories.php"
+
+    try {
+        const response = await fetch(mealCategoriesApiURL)
+        const data = response.json()
+        console.log('data: ', data)
+
+    } catch (error) {
+        console.log(error)
+        alert("Something went wrong, try again later")
+    }
+
+}
+
 const handleFormInputFocus = async (event) => {
     console.log("focus occured")
     await getMealCategories()
