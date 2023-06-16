d9874a91a37d94ed0770b56a1155028b51878763
Yer Yang <yyhmg_86@yahoo.com>
2023-06-15 22:09:57 -0700

    add pseudocode for renderMealCategories

--- main.js
+++ main.js
@@ -20,16 +20,36 @@ const getMealCategories = async () => {
 }
 
 // render meal categories data to dom
-const renderMealCategories = (mealCategoriesObj) => {
+const renderMealCategories = (mealCategoriesArray) => {
     console.log("renderMealCategories")
-    console.table(mealCategoriesObj)
+    console.table(mealCategoriesArray)
+
+    // find and select a UL with meal categories ID dom element to append my data into
+    // for each element in our meal categories array
+    // create a wrapping element maybe <article> class of card
+    // appendChild the article.card to selected DOM 
+    // display the category name, image, and description
+    // image
+    // create an img element
+    // set img src to category thumbnail url
+    // give class of category-thumbnail
+    // give it a mobile friendly max width of like 300px
+    // appendChild the image to our card
+    // name
+    // create an h4 element
+    // set h4 element textContent to be meal category name
+    // appendChild the h4 to our card
+    // description
+    // create a p element
+    // set p element textContent to be meal category description
+    // appendChild the p to our card
 }
 
 // function to call when form input is given focused
 const handleFormInputFocus = async (event) => {
     console.log("focus occured")
-    const mealCategoriesObj = await getMealCategories()
-    renderMealCategories(mealCategoriesObj)
+    const mealCategoriesArray = await getMealCategories()
+    renderMealCategories(mealCategoriesArray)
 }
 
 // add event listener to search term input
