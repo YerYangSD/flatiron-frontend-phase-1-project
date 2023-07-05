020b590678297eb01145ab2c64dd689fcea0869f
Yer Yang <yyhmg_86@yahoo.com>
2023-07-05 10:52:35 -0700

    adds fruit names to second dropdown

--- javascript/main.js
+++ javascript/main.js
@@ -5,8 +5,12 @@ fetch("http://localhost:3000/fruits")
         fruitData.forEach(fruit => {
             // console.log(fruit.name)
             const selection1 = document.querySelector("#selection-1")
-            const option = document.createElement("option")
-            option.textContent = fruit.name
-            selection1.append(option)
+            const selection2 = document.querySelector("#selection-2")
+            const option1 = document.createElement("option")
+            const option2 = document.createElement("option")
+            option1.textContent = fruit.name
+            option2.textContent = fruit.name
+            selection1.append(option1)
+            selection2.append(option2)
         });
     })
