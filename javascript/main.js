32e105a540e495b86dee949a56a80f3c53cbe304
Yer Yang <yyhmg_86@yahoo.com>
2023-07-20 14:45:02 -0700

    uncomments unused code in main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,10 @@
 // Select Elements
 const selection1 = document.querySelector("#selection-1")
-const selection2 = document.querySelector("#selection-2")
+// const selection2 = document.querySelector("#selection-2")
 const fruitContainer = document.querySelector("#fruit-container")
-const secondImageContainer = document.querySelector("#second-image-container")
+// const secondImageContainer = document.querySelector("#second-image-container")
 // const firstImage = document.querySelector("#first-image")
-const secondImage = document.querySelector("#second-image")
+// const secondImage = document.querySelector("#second-image")
 // const firstFruitName = document.querySelector("#first-fruit-name")
 // const firstNutritions = document.querySelector("#first-nutritions")
 // const firstFruitCaloiries = document.querySelector("#first-fruit-caloiries")
@@ -12,15 +12,15 @@ const secondImage = document.querySelector("#second-image")
 // const firstFruitSugar = document.querySelector("#first-fruit-sugar")
 // const firstFruitCarbohydrates = document.querySelector("#first-fruit-carbohydrates")
 // const firstFruitProtein = document.querySelector("#first-fruit-protein")
-const secondFruitName = document.querySelector("#second-fruit-name")
-const secondNutritions = document.querySelector("#second-nutritions")
-const secondFruitCaloiries = document.querySelector("#second-fruit-caloiries")
-const secondFruitFat = document.querySelector("#second-fruit-fat")
-const secondFruitSugar = document.querySelector("#second-fruit-sugar")
-const secondFruitCarbohydrates = document.querySelector("#second-fruit-carbohydrates")
-const secondFruitProtein = document.querySelector("#second-fruit-protein")
-const firstSearchBar = document.querySelector("#first-search-bar")
-const secondSearchBar = document.querySelector("#second-search-bar")
+// const secondFruitName = document.querySelector("#second-fruit-name")
+// const secondNutritions = document.querySelector("#second-nutritions")
+// const secondFruitCaloiries = document.querySelector("#second-fruit-caloiries")
+// const secondFruitFat = document.querySelector("#second-fruit-fat")
+// const secondFruitSugar = document.querySelector("#second-fruit-sugar")
+// const secondFruitCarbohydrates = document.querySelector("#second-fruit-carbohydrates")
+// const secondFruitProtein = document.querySelector("#second-fruit-protein")
+// const firstSearchBar = document.querySelector("#first-search-bar")
+// const secondSearchBar = document.querySelector("#second-search-bar")
 const multiDropdown = document.querySelector("#multidropdown")
 let fruitArray = []
 
@@ -49,10 +49,10 @@ function renderFruitData(fruitData) {
         // console.log(fruit)
         const option1 = document.createElement("option")
         option1.textContent = fruit.name
-        const option2 = document.createElement("option")
-        option2.textContent = fruit.name
+        // const option2 = document.createElement("option")
+        // option2.textContent = fruit.name
         selection1.append(option1)
-        selection2.append(option2)
+        // selection2.append(option2)
 
         // selection1.addEventListener("change", event => {
         //     // console.log(event.target.value)
@@ -86,48 +86,41 @@ function renderFruitData(fruitData) {
     });
 }
 
-firstSearchBar.addEventListener("search", event => {
-    const fruitName = event.target.value
-    const lowercasedFruitName = fruitName.toString().toLowerCase()
-    const results = fruitArray.filter(fruit => fruit.name.toLowerCase().includes(lowercasedFruitName))
-    renderFruits(results)
-})
+// function renderFruits(results) {
+//     if (results.length === 0) {
+//         alert("No Fruits Found")
+//     } else {
+//         results.forEach(fruit => renderFruit(fruit))
+//     }
+// }
 
-function renderFruits(results) {
-    if (results.length === 0) {
-        alert("No Fruits Found")
-    } else {
-        results.forEach(fruit => renderFruit(fruit))
-    }
-}
+// function renderFruit(fruit) {
+//     // const fruitName = document.createElement("h2")
+//     // const fruitImage = document.createElement("img")
+//     // const nutritionInfo = document.createElement("ul")
+//     // const fruitCaloiries = document.createElement("li")
+//     // const fruitFat = document.createElement("li")
+//     // const fruitSugar = document.createElement("li")
+//     // const fruitCarbohydrates = document.createElement("li")
+//     // const fruitProtein = document.createElement("li")
+//     // const button = document.createElement("button")
 
-function renderFruit(fruit) {
-    const fruitName = document.createElement("h2")
-    const fruitImage = document.createElement("img")
-    const nutritionInfo = document.createElement("ul")
-    const fruitCaloiries = document.createElement("li")
-    const fruitFat = document.createElement("li")
-    const fruitSugar = document.createElement("li")
-    const fruitCarbohydrates = document.createElement("li")
-    const fruitProtein = document.createElement("li")
-    const button = document.createElement("button")
+//     // fruitName.textContent = fruit.name
+//     // fruitImage.src = fruit.image
+//     // fruitImage.alt = `This is a picture of a ${fruit.name}`
+//     // button.type = "button"
+//     // button.textContent = "Hide Nutrition Info"
+//     // fruitCaloiries.textContent = `Calories: ${fruit.nutritions.calories}`
+//     // fruitFat.textContent = `Fat: ${fruit.nutritions.fat}`
+//     // fruitSugar.textContent = `Sugar: ${fruit.nutritions.sugar}`
+//     // fruitCarbohydrates.textContent = `Carboydrates: ${fruit.nutritions.carbohydrates}`
+//     // fruitProtein.textContent = `Protein: ${fruit.nutritions.protein}`
+//     // nutritionInfo.append(fruitCaloiries, fruitFat, fruitSugar, fruitCarbohydrates, fruitProtein)
+//     // fruitContainer.append(fruitName, fruitImage, button, nutritionInfo)
+//     // console.log(fruitContainer)
 
-    fruitName.textContent = fruit.name
-    fruitImage.src = fruit.image
-    fruitImage.alt = `This is a picture of a ${fruit.name}`
-    button.type = "button"
-    button.textContent = "Hide Nutrition Info"
-    fruitCaloiries.textContent = `Calories: ${fruit.nutritions.calories}`
-    fruitFat.textContent = `Fat: ${fruit.nutritions.fat}`
-    fruitSugar.textContent = `Sugar: ${fruit.nutritions.sugar}`
-    fruitCarbohydrates.textContent = `Carboydrates: ${fruit.nutritions.carbohydrates}`
-    fruitProtein.textContent = `Protein: ${fruit.nutritions.protein}`
-    nutritionInfo.append(fruitCaloiries, fruitFat, fruitSugar, fruitCarbohydrates, fruitProtein)
-    fruitContainer.append(fruitName, fruitImage, button, nutritionInfo)
-    // console.log(fruitContainer)
-
-    button.addEventListener("click", event => toggleNutritionValues(event, nutritionInfo, button))
-}
+//     button.addEventListener("click", event => toggleNutritionValues(event, nutritionInfo, button))
+// }
 
 multiDropdown.addEventListener("submit", getMultiFruits)
 
